439aca7e8f2643c6e27e270890e4b8bbf6a40101
UsaidAl <ali_usaid98@hotmail.com>
2024-03-13 23:11:31 +0000

    prompt message and choices

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const inquirer = require('inquirer');
 const database = require('./db/database')
 
+
+async function app() {
+    let continueExecution = true;
+
+    while (continueExecution) {
+        const {action} = await inquirer.prompt({
+            type: 'list',
+            name: 'action',
+            message: 'How can we help?',
+            choices: [
+                'Show All Departments',
+                'Show All Roles',
+                'Show All Employees',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Change an Employee Role',
+                'Change an Employees Manager',
+                'Remove a Department',
+                'Remove a Role',
+                'Remove an Employee',
+                'Quit Application'
+            ],
+        });
+    }
+};
